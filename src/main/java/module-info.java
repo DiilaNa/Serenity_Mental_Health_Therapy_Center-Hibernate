5bd74587c610bca1519bb542f091d3aa98d6d74e
DiilaNa <liyanaarachchidilan@gmail.com>
2025-03-12 20:23:04 +0530

    controller opens to javafx inside module info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,8 +3,9 @@ module lk.ijse.project.mentalhealththerapycenter {
     requires javafx.fxml;
     requires java.sql;
 
-
-
     opens lk.ijse.project.mentalhealththerapycenter to javafx.fxml;
     exports lk.ijse.project.mentalhealththerapycenter;
+    exports lk.ijse.project.mentalhealththerapycenter.controller;
+    opens lk.ijse.project.mentalhealththerapycenter.controller to javafx.fxml;
+    /*tm eka javafx base ekata open krnna*/
 }
