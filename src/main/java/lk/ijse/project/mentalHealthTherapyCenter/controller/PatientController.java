baac0f158bfd43c88e4380e431cf2e54a99bd9d3
DiilaNa <liyanaarachchidilan@gmail.com>
2025-03-17 17:48:57 +0530

    image added in ui and patient contoller

--- src/main/java/lk/ijse/project/mentalHealthTherapyCenter/controller/PatientController.java
+++ src/main/java/lk/ijse/project/mentalHealthTherapyCenter/controller/PatientController.java
@@ -2,12 +2,23 @@ package lk.ijse.project.mentalHealthTherapyCenter.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 
-public class PatientController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PatientController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Image image1 = new Image(getClass().getResourceAsStream("/images/patient.png"));
+        image.setImage(image1);
+    }
 
     @FXML
     private Button delete;
@@ -41,6 +52,9 @@ public class PatientController {
 
     @FXML
     private Button update;
+
+    @FXML
+    private ImageView image;
 
     @FXML
     void deleteAction(ActionEvent event) {
