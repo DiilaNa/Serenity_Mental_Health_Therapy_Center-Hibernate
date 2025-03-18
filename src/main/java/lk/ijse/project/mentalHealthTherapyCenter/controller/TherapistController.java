aa2f69357f97c909d153709ae8056c908b87898e
DiilaNa <liyanaarachchidilan@gmail.com>
2025-03-18 14:00:47 +0530

    therapist ui, controller added

--- src/main/java/lk/ijse/project/mentalHealthTherapyCenter/controller/TherapistController.java
+++ src/main/java/lk/ijse/project/mentalHealthTherapyCenter/controller/TherapistController.java
@@ -1,4 +1,101 @@
 package lk.ijse.project.mentalHealthTherapyCenter.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
 public class TherapistController {
+
+    @FXML
+    private Button delete;
+
+    @FXML
+    private TextField docAddress;
+
+    @FXML
+    private TextField docContact;
+
+    @FXML
+    private Label docIDlabel;
+
+    @FXML
+    private TextField docMail;
+
+    @FXML
+    private TextField docName;
+
+    @FXML
+    private TextField docNic;
+
+    @FXML
+    private ComboBox<?> docQualifications;
+
+    @FXML
+    private ImageView image;
+
+    @FXML
+    private Button reset;
+
+    @FXML
+    private Button save;
+
+    @FXML
+    private TableView<?> table;
+
+    @FXML
+    private TableColumn<?, ?> tableAddress;
+
+    @FXML
+    private TableColumn<?, ?> tableContact;
+
+    @FXML
+    private TableColumn<?, ?> tableId;
+
+    @FXML
+    private TableColumn<?, ?> tableMail;
+
+    @FXML
+    private TableColumn<?, ?> tableName;
+
+    @FXML
+    private TableColumn<?, ?> tableNic;
+
+    @FXML
+    private TableColumn<?, ?> tableQualifications;
+
+    @FXML
+    private Button update;
+
+    @FXML
+    void TableAction(MouseEvent event) {
+
+    }
+
+    @FXML
+    void deleteBtnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void resetBtnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void saveBtnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void updateBtnAction(ActionEvent event) {
+
+    }
+
 }
