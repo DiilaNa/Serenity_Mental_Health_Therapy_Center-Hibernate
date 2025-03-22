cf437766cbb371df9288c804cd6fe26b6005f694
DiilaNa <liyanaarachchidilan@gmail.com>
2025-03-22 18:48:04 +0530

    PaymentTM added

---
+++ src/main/java/lk/ijse/project/mentalHealthTherapyCenter/dto/TM/PaymentTM.java
@@ -0,0 +1,16 @@
+package lk.ijse.project.mentalHealthTherapyCenter.dto.TM;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PaymentTM {
+    private  String paymentID;
+    private  Double paymentAmount;
+    private  String paymentMethod;
+    private  String paymentDate;
+    private  String paymentTime;
+}
