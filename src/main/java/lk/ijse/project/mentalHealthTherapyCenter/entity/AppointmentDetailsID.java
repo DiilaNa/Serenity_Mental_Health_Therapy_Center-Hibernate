ca0e83f4a6c32bf85ed4f0b5b1eefb1c72cf1420
DiilaNa <liyanaarachchidilan@gmail.com>
2025-03-27 19:45:56 +0530

    equals and hashcode added in entity id classes

--- src/main/java/lk/ijse/project/mentalHealthTherapyCenter/entity/AppointmentDetailsID.java
+++ src/main/java/lk/ijse/project/mentalHealthTherapyCenter/entity/AppointmentDetailsID.java
@@ -2,6 +2,7 @@ package lk.ijse.project.mentalHealthTherapyCenter.entity;
 
 import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
@@ -10,6 +11,7 @@ import java.io.Serializable;
 
 @AllArgsConstructor
 @NoArgsConstructor
+@EqualsAndHashCode
 @Embeddable
 public class AppointmentDetailsID implements Serializable {
     private String appointmentId;
--- src/main/java/lk/ijse/project/mentalHealthTherapyCenter/entity/ProgramDetailsID.java
+++ src/main/java/lk/ijse/project/mentalHealthTherapyCenter/entity/ProgramDetailsID.java
@@ -2,13 +2,14 @@ package lk.ijse.project.mentalHealthTherapyCenter.entity;
 
 import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @AllArgsConstructor
 @NoArgsConstructor
-
+@EqualsAndHashCode
 @Embeddable
 public class ProgramDetailsID implements Serializable {
     private String therapyProgramID;
